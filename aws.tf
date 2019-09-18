8873f499f2f6a5fd8ef2d02253fcd87d1c2fbc45
ILAN <ilan.durairaj@sydney.edu.au>
2019-09-18 11:07:32 +1000

    Started Learning Terraform second time

--- aws.tf
+++ aws.tf
@@ -1,13 +1,18 @@
 provider "aws" {
-   access_key = "AKIAJGVNPR6UZZNSNISA"
-   secret_key = "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"
+   access_key = "AKIAQKOVO6KJ6XSUUYM5"
+   secret_key = "mwgW9KGAN8Qc3VC7sBNXFXcF7rSPfGlP9ehPyDp5"
    region     = "ap-southeast-2"
 }
 
 resource "aws_instance" "aws_example"  {
-  ami           = "ami-09b42976632b27e9b"
+  ami           = "ami-07cc15c3ba6f8e287"
   instance_type = "t2.micro"
-  
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, WOrld" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags {
   Name = "ilanterraform-example"
 }  
