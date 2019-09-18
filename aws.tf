32b926beb19e91c08c34110c771a4d20034ca4cc
ILAN <ilan.durairaj@sydney.edu.au>
2019-09-18 15:52:54 +1000

    added ALB, ASG and LAUNCH CONFIG

--- aws.tf
+++ aws.tf
@@ -1,6 +1,8 @@
+data "aws_availability_zones" "all" {}
+
 variable "server_port" {
   description = "Server Port used by terraform"
-  default = 8080
+  default = 3000
 }
 
 resource "aws_security_group" "tesrinstance" {
@@ -11,20 +13,85 @@ resource "aws_security_group" "tesrinstance" {
     to_port = "${var.server_port}"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_instance" "aws_example"  {
-  ami           = "ami-07cc15c3ba6f8e287"
+resource "aws_launch_configuration" "example"  {
+  image_id = "ami-07cc15c3ba6f8e287"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.tesrinstance.id}"]
+  security_groups = ["${aws_security_group.tesrinstance.id}"]
+
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, WOrld" > index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
+              sudo yum install --enablerepo=epel -y nodejs
+              wget http://bit.ly/2vESNuc -O /home/ec2-user/helloworld.js
+              wget http://bit.ly/2vVvT18 -O /etc/init/helloworld.conf
+              start helloworld
               EOF
-  tags {
-    Name = "ilanterraform-seoondtrystudy"
+  lifecycle {
+    create_before_destroy = true
   }
 
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  max_size = 10
+  min_size = 2
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+
+  tag {
+    key = "Name"
+    propagate_at_launch = true
+    value = "terraform-asg-example"
+  }
+
+}
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+  "listener" {
+    instance_port = "${var.server_port}"
+    instance_protocol = "HTTP"
+    lb_port = 80
+    lb_protocol = "HTTP"
+  }
+  health_check {
+    healthy_threshold = 2
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+    timeout = 3
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
