f34d95b09a47a9206cceed18816b16b62628cbbc
ILAN <ilan.durairaj@sydney.edu.au>
2019-09-18 11:31:30 +1000

    remove

--- aws.tf
+++ aws.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-  access_key = "AKIAQKOVO6KJ6XSUUYM5"
-  secret_key = "mwgW9KGAN8Qc3VC7sBNXFXcF7rSPfGlP9ehPyDp5"
-  region     = "ap-southeast-2"
+
+
 }
+
+
 
 resource "aws_instance" "aws_example"  {
   ami           = "ami-07cc15c3ba6f8e287"
